ac5f9adf62752281fee12666c882866f1d1195a7
danielrosenthal2 <rosedb19@wfu.edu>
2021-04-26 17:47:25 -0400

    commit 1

--- src/Iris.java
+++ src/Iris.java
@@ -1,2 +1,88 @@
-public class Iris {
+import java.util.Comparator;
+
+public class Iris implements Comparable<Iris>{
+    protected double sepal_length;
+    protected double sepal_width;
+    protected double petal_length;
+    protected double petal_width;
+    protected String species;
+
+    //default constructor
+    public Iris() {
+        double sepal_length;
+        double sepal_width;
+        double petal_length;
+        double petal_width;
+        String species;
+    }
+
+    //parametrized constructor
+    public Iris(double sepal_length, double sepal_width, double petal_length, double petal_width, String species) {
+        this.sepal_length = sepal_length;
+        this.sepal_width = sepal_width;
+        this.petal_length = petal_length;
+        this.petal_width = petal_width;
+        this.species = species;
+    }
+    //copy constructor
+    public Iris(Iris i) {
+        this.sepal_length = i.sepal_length;
+        this.sepal_width = i.sepal_width;
+        this.petal_length = i.petal_length;
+        this.petal_width = i.petal_width;
+    }
+
+    //getters
+    public double getSepal_length() {
+        return sepal_length;
+    }
+    public double getSepal_width() {
+        return sepal_width;
+    }
+    public double getPetal_length() {
+        return petal_length;
+    }
+    public double getPetal_width() {
+        return petal_width;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    //setters
+    public void setSepal_length(double sepal_length) {
+        this.sepal_length = sepal_length;
+    }
+
+    public void setSepal_width(double sepal_width) {
+        this.sepal_width = sepal_width;
+    }
+    public void setPetal_length(double petal_length) {
+        this.petal_length = petal_length;
+    }
+    public void setPetal_width(double petal_width) {
+        this.petal_width = petal_width;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    //print statement that is not used b/c there is too much data
+    public String toString() {
+        return("The flower is a " + species + " with a sepal length of " + sepal_length + " and sepal width of " +
+                + sepal_width + ", and a petal width of " + petal_width + " and petal length of " +
+                + petal_length + ".");
+    }
+
+    //comparable compares current sepal length first and then petal length if sepal is equal
+    @Override
+    public int compareTo(Iris o) {
+        int sepal_lengthCompare = Double.compare(sepal_length, o.sepal_length);
+        int petal_lengthCompare = Double.compare(petal_length, o.petal_length);
+        return ((sepal_lengthCompare == 0) ? petal_lengthCompare : sepal_lengthCompare);
+    }
+
+    //
+    boolean isLessThan(Iris Iris2) {
+        return(this.compareTo(Iris2) < 0);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,76 @@
+import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
 
     int binarySearch(LinkedList a, Iris item, int low, int high) {
+
         return 0;
     }
 
     void insertionSort(LinkedList a, int n) {
+        int i = 0;
+        while (i < a.size()) {
 
+        }
     }
 
-        public static void main(String [] args){
+    public static String loop(int listSize) {
+        //file reader
+        FileInputStream fis = null;
+        String lines[];
+        //try and catch for file not found, initializes fis
+        try {
+            fis = new FileInputStream("fish-iris.csv.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+
+        ArrayList<Iris> arraylist = new ArrayList<Iris>(); //arraylist for sorting
+        LinkedList<Iris> linkedlist = new LinkedList<>(); //linkedlist for sorting
+
+        //fill list
+        Scanner reader = new Scanner(fis);
+        reader.nextLine(); //skips column headers
+        //reader iterates thru list certain number of times, splits by comma and parses and adds to arraylist
+        for (int i = 0; i < listSize; i++) {
+            lines = reader.nextLine().split(",");
+            arraylist.add(new Iris(Double.parseDouble(lines[0]), Double.parseDouble(lines[1]), Double.parseDouble(lines[2]),
+                    Double.parseDouble(lines[3]), lines[4]));
+            //test1
+
+        }
+
+        //reader iterates thru list certain number of times, splits by comma and parses and adds to linkedlist
+        for (int i = 0; i < listSize; i ++) {
+            lines = reader.nextLine().split(",");
+            linkedlist.add(new Iris(Double.parseDouble(lines[0]), Double.parseDouble(lines[1]), Double.parseDouble(lines[2]),
+                    Double.parseDouble(lines[3]), lines[4]));
+        }
+        reader.close();
+
+        //test
+        String print;
+        print = linkedlist.get(11).toString();
+        return print;
+    }
+
+    public static void main(String[] args) {
+//fileoutputstream to write new file
+        FileOutputStream fos = null;
+        File csvFile = new File("runtime.txt");
+        try {
+            csvFile.createNewFile();
+            PrintWriter writer = new PrintWriter(csvFile);
+            writer.println(loop(1000));
+
+
+            writer.close();
+        } catch (IOException e){
+            System.exit(1);
+        }
     }
 }
